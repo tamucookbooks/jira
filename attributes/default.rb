bc997f00b201f86fc6cd10d328ff637377fdbcbb
Jim Rosser <jarosser06@gmail.com>
2013-05-29 10:52:40 -0500

    added some default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+default[:jira][:download_url] = "http://www.atlassian.com/software/jira/downloads/binary/#{node[:jira][:package]}"
+default[:jira][:version] = "6.0.1"
+default[:jira][:package] = "atlassian-jira-#{node[:jira][:version]}.tar.gz" 
+
+default[:jira][:user] = "jira"
+default[:jira][:group] = "jira"
+default[:jira][:install_dir] = "/opt/jira"
+default[:jira][:home_dir] = "/opt/jira_home"
+
+
