a4ff554d2628d914aacba7809b846da328426cc8
Jim Rosser <jarosser06@gmail.com>
2013-06-07 11:51:55 -0500

    split out important directories

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,12 @@ default[:jira][:version] = "6.0.1"
 default[:jira][:package] = "atlassian-jira-#{node[:jira][:version]}.tar.gz"
 
 default[:jira][:download_url] = "http://downloads.atlassian.com/software/jira/downloads/#{node[:jira][:package]}"
-
 default[:jira][:user] = "jira"
 default[:jira][:group] = "jira"
-default[:jira][:install_dir] = "/opt/jira"
-default[:jira][:home_dir] = "/opt/jira_home"
+
+# base dir to store jira 
+default[:jira][:dir] = "/usr/local/jira"
+# home directory stores configs, and things specific to instance
+default[:jira][:home_dir] = "#{node[:jira][:dir]}/jira_home"
+# directory jira is extracted to
+default[:jira][:app_dir] = "#{node[:jira][:dir]}/jira"
