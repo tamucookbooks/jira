f1e95ae1fc987550ba7a54d698811aeebaf22056
Jim Rosser <jarosser06@gmail.com>
2013-06-07 11:53:53 -0500

    jira service

---
+++ recipes/service.rb
@@ -0,0 +1,11 @@
+template "/etc/init.d/jira" do
+  action :create
+  source "init/jira.erb"
+  mode "0755"
+  notifies :restart, "service[jira]", :delayed
+end
+
+service "jira" do
+  supports :start => true, :restart => true
+  action [:enable, :start]
+end
