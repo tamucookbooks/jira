807981d2b820896bb1e837de2aea322f15bc5363
Victoria Blessing <vblessing@arch.tamu.edu>
2015-11-03 10:30:47 -0600

    add 14.04 to test kitchen

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 description      'Installs/Configures jira'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.2'
-supports         'ubuntu', '>= 12.04'
+supports         'ubuntu', '>= 14.04'
 
 %w{ ark postgresql mysql }.each do |dep|
   depends dep
