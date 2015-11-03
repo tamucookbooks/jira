e22e441d39689e9bc4d8fc6dddd804fae2f77cfd
Victoria Blessing <vblessing@arch.tamu.edu>
2015-11-03 09:42:38 -0600

    remove unnecessary database cookbook

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.2'
 supports         'ubuntu', '>= 12.04'
 
-%w{ ark postgresql mysql database }.each do |dep|
+%w{ ark postgresql mysql }.each do |dep|
   depends dep
 end
